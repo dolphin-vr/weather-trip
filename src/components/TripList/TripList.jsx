bb4572b940c8cfcc8f6efbe9f54aeae0dfe0ecbf
dolphin <fun53@ukr.net>
2024-02-27 01:15:11 +0200

    wheather api

--- src/components/TripList/TripList.jsx
+++ src/components/TripList/TripList.jsx
@@ -1,10 +1,10 @@
 import { TripCard } from "../TripCard/TripCard";
 import { List } from "./TripList.styled";
 
-export const TripList = ({trips}) => {
+export const TripList = ({trips, handleClick}) => {
 	return (
 			<List>
-				{trips.map(el=><TripCard  key={el.id} trip={el} />)}
+			{trips.map(el => <TripCard key={el.id} trip={el} onClick={handleClick} />)}
 			</List>
 	)
 };
---
+++ src/shared/weaterApi.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { dateISO } from "./utils/dates";
+
+const requester = axios.create({
+  baseURL: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/",
+  headers: { "content-type": "application/json" },
+	params: {
+		unitGroup: "metric",
+		include: "day",
+		contentType: "json",
+    key: import.meta.env.VITE_WEATHERAPI_KEY,
+  },
+});
+
+const getToday = async (city) => {
+	const { data } = await requester.get(`${city.replaceAll(" ", "-")}/today`);
+	return data
+}
+
+const getRange = async (city, startDate, endDate) => {
+  const { data } = await requester.get(`${city.replaceAll(" ", "-")}/${dateISO(startDate)}/${dateISO(endDate)}`);
+  return data;
+};
+
+export default { getToday, getRange };
